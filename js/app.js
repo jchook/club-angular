44d8162fe95e5a84e49e107c7431a1f04386bad0
Wes Roberts <wes@sdynamics.com>
2015-06-04 23:40:42 -0400

    v.1.0

---
+++ js/app.js
@@ -0,0 +1,87 @@
+// Wes Roberts
+// 2015-06-03
+//
+angular
+
+	// Club Angular
+	.module('clubAngular', ['ngAnimate', 'ui.bootstrap'])
+
+	// Member directory
+	.controller('memberDirectoryCtl', ['$scope', function($scope){
+
+		// Import data.js
+		$scope.nameFilter = '';
+		$scope.stateFilter = '';
+		$scope.members = memberDirectoryData;
+		$scope.membersFiltered = $scope.members.slice();
+		$scope.membersPaginated = $scope.members.slice(0,10);
+		$scope.currentPage = 1;
+		$scope.numPerPage = 10;
+
+		$scope.$watch('nameFilter + stateFilter', function(){
+			$scope.updateFilters();
+			$scope.updatePagination();
+		});
+		$scope.$watch('currentPage + numPerPage', function(){
+			$scope.updatePagination();
+		});
+
+		$scope.updateFilters = function() {
+			var i, r = [];
+			for (i=0; i<$scope.members.length; i++) {
+				if ($scope.applyFilters($scope.members[i])) {
+					r.push($scope.members[i]);
+				}
+			}
+			$scope.membersFiltered = r;
+		};
+		$scope.updatePagination = function() {
+			var begin, end;
+			$scope.numPerPage = parseInt($scope.numPerPage);
+			begin = (($scope.currentPage - 1) * $scope.numPerPage);
+			end = begin + $scope.numPerPage;
+			$scope.membersPaginated = $scope.membersFiltered.slice(begin, end);
+		};
+
+		$scope.maxPage = function() {
+			return Math.ceil($scope.membersFiltered.length / $scope.numPerPage);
+		};
+
+		$scope.getPages = function () {
+			var end = $scope.maxPage(), i, ret = [];
+			for (i = 1; i <= end; i++) {
+				ret.push(i);
+			}
+			return ret;
+		};
+		
+		$scope.prevPage = function () {
+			if ($scope.currentPage > 1) {
+				$scope.currentPage--;
+			}
+		};
+		
+		$scope.nextPage = function () {
+			if ($scope.currentPage < $scope.maxPage()) {
+				$scope.currentPage++;
+			}
+		};
+		
+		$scope.setPage = function (n) {
+			$scope.currentPage = n;
+		};
+
+
+		// Filter the directory
+		$scope.applyFilters = function(member) {
+			return true
+
+				// First or last name match
+				&& (member.first_name.match($scope.nameFilter) || member.last_name.match($scope.nameFilter))
+
+				// State match required
+				&& member.state.match($scope.stateFilter)
+			;
+		};
+	}])
+;
