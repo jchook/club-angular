4fea995627d4a31350d7f66a669fd50f1fb59a0e
Wes Roberts <wes@sdynamics.com>
2015-06-09 02:49:05 -0400

    Clean-up

--- js/app.js
+++ js/app.js
@@ -1,92 +1,96 @@
 // Wes Roberts
 // 2015-06-04
 //
-angular
+(function(){
 
-	// Club Angular
-	.module('clubAngular', [])
+	angular
 
-	// Member directory
-	.controller('memberDirectoryCtl', ['$scope', function($scope){
+		// Club Angular
+		.module('clubAngular', [])
 
-		// Pagination & Filters
-		angular.extend($scope, {
-			
-			numPerPage: 10,
-			currentPage: 1,
-			
-			members: memberDirectoryData,
-			membersFiltered: memberDirectoryData,
-			membersPaginated: memberDirectoryData.slice(0,10),
+		// Member directory
+		.controller('memberDirectoryCtl', ['$scope', function($scope){
 
-			nameFilter: '',
-			stateFilter: '',
+			// Pagination & Filters
+			angular.extend($scope, {
+				
+				numPerPage: 10,
+				currentPage: 1,
+				
+				members: memberDirectoryData,
+				membersFiltered: memberDirectoryData,
+				membersPaginated: memberDirectoryData.slice(0,10),
 
-			applyFilters: function(member) {
-				var i, j, keywords, nameMatch;
+				nameFilter: '',
+				stateFilter: '',
 
-				// Match all words
-				keywords = $scope.nameFilter.split(' ');
-				nameMatch = true;
-				for (i=0, j=keywords.length; i<j; i++) {
-					nameMatch = nameMatch && [member.first_name, member.last_name].join(' ').match(keywords[i]);
-				}
+				applyFilters: function(member) {
+					var i, j, keywords, nameMatch;
 
-				return (j=0 || nameMatch) && member.state.match($scope.stateFilter);
-			},
-
-			getPages: function() {
-				var end = $scope.maxPage(), i, ret = [];
-				for (i = 1; i <= end; i++) {
-					ret.push(i);
-				}
-				return ret;
-			},
-
-			maxPage: function() {
-				return Math.ceil($scope.membersFiltered.length / $scope.numPerPage) || 1;
-			},
-
-			nextPage: function() {
-				if ($scope.currentPage < $scope.maxPage()) {
-					$scope.currentPage++;
-				}
-			},
-			
-			prevPage: function() {
-				if ($scope.currentPage > 1) {
-					$scope.currentPage--;
-				}
-			},
-			
-			setPage: function(n) {
-				$scope.currentPage = n;
-			},
-
-			updateFilters: function() {
-				var i, r = [];
-				for (i=0; i<$scope.members.length; i++) {
-					if ($scope.applyFilters($scope.members[i])) {
-						r.push($scope.members[i]);
+					// Match all words
+					keywords = $scope.nameFilter.split(' ');
+					nameMatch = true;
+					for (i=0, j=keywords.length; i<j; i++) {
+						nameMatch = nameMatch && [member.first_name, member.last_name].join(' ').match(keywords[i]);
 					}
-				}
-				$scope.membersFiltered = r;
-				$scope.updatePagination();
-			},
 
-			updatePagination: function() {
-				var begin, end;
-				$scope.numPerPage = parseInt($scope.numPerPage);
-				$scope.currentPage = Math.min($scope.currentPage, $scope.maxPage());
-				begin = (($scope.currentPage - 1) * $scope.numPerPage);
-				end = begin + $scope.numPerPage;
-				$scope.membersPaginated = $scope.membersFiltered.slice(begin, end);
-			},
-		});
-		
-		// Observers
-		$scope.$watch('nameFilter + stateFilter', $scope.updateFilters);
-		$scope.$watch('currentPage + numPerPage', $scope.updatePagination);
+					return (j=0 || nameMatch) && member.state.match($scope.stateFilter);
+				},
 
-	}])
-;
+				getPages: function() {
+					var i, j, pages = [];
+					for (i=1, j=$scope.maxPage(); i<=j; i++) {
+						pages.push(i);
+					}
+					return pages;
+				},
+
+				maxPage: function() {
+					return Math.ceil($scope.membersFiltered.length / $scope.numPerPage) || 1;
+				},
+
+				nextPage: function() {
+					if ($scope.currentPage < $scope.maxPage()) {
+						$scope.currentPage++;
+					}
+				},
+				
+				prevPage: function() {
+					if ($scope.currentPage > 1) {
+						$scope.currentPage--;
+					}
+				},
+				
+				setPage: function(n) {
+					$scope.currentPage = n;
+				},
+
+				updateFilters: function() {
+					var i, r = [];
+					for (i=0; i<$scope.members.length; i++) {
+						if ($scope.applyFilters($scope.members[i])) {
+							r.push($scope.members[i]);
+						}
+					}
+					$scope.membersFiltered = r;
+					$scope.updatePagination();
+				},
+
+				updatePagination: function() {
+					var begin, end;
+					$scope.numPerPage = parseInt($scope.numPerPage);
+					$scope.currentPage = Math.min($scope.currentPage, $scope.maxPage());
+					begin = (($scope.currentPage - 1) * $scope.numPerPage);
+					end = begin + $scope.numPerPage;
+					$scope.membersPaginated = $scope.membersFiltered.slice(begin, end);
+				},
+			});
+			
+			// Observers
+			$scope.$watch('nameFilter + stateFilter', $scope.updateFilters);
+			$scope.$watch('currentPage + numPerPage', $scope.updatePagination);
+
+		}])
+	;
+
+})();
